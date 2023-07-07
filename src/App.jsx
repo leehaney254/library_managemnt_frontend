f5093b6d7e97ba0310a400213ed504ccd0b6ed20
Leehaney gee <georgeleehaney20@gmail.com>
2023-07-07 11:47:30 +0300

    Feat: Add main routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import Loader from './components/loader/Loader';
+const AllBooks = lazy(() => import('./pages/books/AllBooks'));
+const AllMembers = lazy(() => import('./pages/members/AllMembers'));
+const AllReservations = lazy(() => import('./pages/reservations/AllReservations'));
 import './App.css'
 
 function App() {
   return (
-    <Loader />
+    <Suspense fallback={
+      <Loader />
+    }>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<AllBooks />} />
+          <Route path='/' element={<AllMembers />} />
+          <Route path='/' element={<AllReservations />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   )
 }
 
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export * from './books';
+export * from './members';
+export * from './reservations';
