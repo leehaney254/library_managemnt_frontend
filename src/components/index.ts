ed339a355bc4f37ea566030be4ee3b895acd52fd
Leehaney gee <georgeleehaney20@gmail.com>
2023-07-08 13:45:07 +0300

    Feat: Create info card

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './loader';
 export * from './maincard';
 export * from './navbar';
+export * from './card';
--- src/pages/books/AllBooks.jsx
+++ src/pages/books/AllBooks.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar';
 import MainCard from '../../components/maincard/MainCard';
+import Card from '../../components/card/Card';
 const AllBooks = () => {
   return (
     <main className="flex relative">
@@ -10,6 +11,15 @@ const AllBooks = () => {
         <div className="flex justify-between my-6">
           <input type="text" placeholder="Find by title" className="all_inputs" />
           <input type="text" placeholder="Find by Author" />
+        </div>
+        <div>
+          <Card
+            image="https://miro.medium.com/v2/resize:fit:588/1*f0znhTHMsMTsq9q-m14VIg.jpeg"
+            item1="Title"
+            item1Val="48 Laws of Power"
+            item2="Cost"
+            item2Val1={50}
+          />
         </div>
       </div>
     </main>
