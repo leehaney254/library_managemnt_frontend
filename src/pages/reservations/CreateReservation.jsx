4ac4f7343002c7575020608ccd11161f1adcba01
Leehaney gee <georgeleehaney20@gmail.com>
2023-07-12 16:44:46 +0300

    Feat: Add member to group

--- src/pages/reservations/CreateReservation.jsx
+++ src/pages/reservations/CreateReservation.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Navbar from '../../components/navbar/Navbar';
 import { fetchBooks } from '../../features/books/books';
+import { fetchMembers } from '../../features/members/members';
 
 const CreateReservation = () => {
 
@@ -10,10 +11,11 @@ const CreateReservation = () => {
   // const navigate = useNavigate();
 
   const books_data = useSelector((state) => state.books.books);
-  const { books } = books_data;
+  const members_data = useSelector((state) => state.member.members);
 
   useEffect(() => {
     dispatch(fetchBooks());
+    dispatch(fetchMembers());
   }, [dispatch])
 
   return (
@@ -22,7 +24,7 @@ const CreateReservation = () => {
       <div className="main_content">
         <form>
           <div className="flex justify-between my-4">
-            {books && <div className="flex flex-col">
+            {books_data && <div className="flex flex-col">
               <label htmlFor="book_id">Select Book:</label>
               <select
                 id="book_id"
@@ -32,7 +34,7 @@ const CreateReservation = () => {
                 <option value="">-- Select a book --</option>
                 {/* Add options dynamically from your book data */}
                 {
-                  books.map((item) => {
+                  books_data.books.map((item) => {
                     return (
                       <option key={item.id} value={item.id} >{item.title}</option>
                     )
@@ -41,20 +43,27 @@ const CreateReservation = () => {
               </select>
             </div>
             }
-            <div className="flex flex-col">
-              <label htmlFor="member_id">Select Member:</label>
-              <select
-                id="member_id"
-                name='member_id'
-                className="all_inputs"
-              >
-                <option value="">-- Select a member --</option>
-                {/* Add options dynamically from your member data */}
-                <option value="member1">Member 1</option>
-                <option value="member2">Member 2</option>
-                <option value="member3">Member 3</option>
-              </select>
-            </div>
+
+            {
+              members_data && <div className="flex flex-col">
+                <label htmlFor="member_id">Select Member:</label>
+                <select
+                  id="member_id"
+                  name='member_id'
+                  className="all_inputs"
+                >
+                  <option value="">-- Select a member --</option>
+                  {/* Add options dynamically from your member data */}
+                  {
+                    members_data.members.map((item) => {
+                      return (
+                        <option key={item.id} value={item.id}>{item.name}</option>
+                      )
+                    })
+                  }
+                </select>
+              </div>
+            }
           </div>
           <div className="flex justify-between my-4">
             <div className="flex flex-col">
