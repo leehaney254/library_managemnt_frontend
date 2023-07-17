e847ad50b160fc5f79176a0358b78080b8bfe3fd
Leehaney gee <georgeleehaney20@gmail.com>
2023-07-17 10:34:17 +0300

    Style: Add style to books info

--- src/components/bookInfoCard/BookInfoCard.jsx
+++ src/components/bookInfoCard/BookInfoCard.jsx
@@ -11,17 +11,17 @@ const BookInfoCard = ({ book_data }) => {
         <div className="flex flex-col">
           <h2 className="font-bold text-xl">Information</h2>
           <hr className="my-4" />
-          <div className="flex justify-between my-2">
-            <p className="flex flex-col"> <span className="font-bold">Title</span>{book_data.title}</p>
-            <p className="flex flex-col"><span className="font-bold">Author</span>{book_data.author}</p>
+          <div className="flex justify-around my-2">
+            <p className="flex flex-col w-32"> <span className="font-bold">Title</span>{book_data.title}</p>
+            <p className="flex flex-col w-32"><span className="font-bold">Author</span>{book_data.author}</p>
           </div>
-          <div className="flex justify-between my-2">
-            <p className="flex flex-col"><span className="font-bold">Publisher</span>{book_data.publisher}</p>
-            <p className="flex flex-col"><span className="font-bold">Publish date</span> {book_data.publication_date}</p>
+          <div className="flex justify-around my-2">
+            <p className="flex flex-col w-32"><span className="font-bold">Publisher</span>{book_data.publisher}</p>
+            <p className="flex flex-col w-32"><span className="font-bold">Publish date</span> {book_data.publication_date}</p>
           </div>
-          <div className="flex justify-between my-2">
-            <p className="flex flex-col"><span className="font-bold">Genre</span> {book_data.genre}</p>
-            <p className="flex flex-col"><span className="font-bold">Count</span>{book_data.amount}</p>
+          <div className="flex justify-around my-2">
+            <p className="flex flex-col w-32"><span className="font-bold">Genre</span> {book_data.genre}</p>
+            <p className="flex flex-col w-32"><span className="font-bold">Count</span>{book_data.amount}</p>
           </div>
         </div>
         <h2 className="font-bold text-xl mt-4">Description</h2>
